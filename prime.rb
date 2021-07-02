3de688b896d1b13a9774ca6fea5a19806db6c802
Cameron <camcreel@gmail.com>
2021-07-01 20:59:47 -0400

    Completed assignment

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+# def factorial(num)
+#     if num == 0
+#         return 1 
+#     end
+
+#     num * factorial(num - 1)
+# end
+
+# def prime?(int)
+    
+#     return false if int < 2
+   
+#     (factorial(int - 1) % int) == (int - 1) 
+    
+# end
+
+
+def prime?(int)
+    return false if int < 2
+    !(2...int).to_a.any? {|num| (int % num) == 0}
+end
